c006c8976665a0f4f20c77e842e750d10c6e1b11
SeoBYP <jajazxcv@naver.com>
2025-04-19 02:26:36 +0900

    port 갱신 다시 실행

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# bash는 return value가 안되니 *제일 마지막줄에 echo로 해서 결과 출력*후, 클라이언트에서 값을 사용한다
+# bash는 return value가 안되니 *제일 마지막줄에 echo로 해서 결과 출력* 후, 클라이언트에서 값을 사용한다
 
 function find_idle_profile()
 {
@@ -11,13 +11,14 @@ function find_idle_profile()
         CURRENT_PROFILE="real2"
     else
         CURRENT_PROFILE=$(curl -s http://localhost/profile | tr -d '\r')
+        echo "> [find_idle_profile] CURRENT_PROFILE 응답 내용: '$CURRENT_PROFILE'"
     fi
 
     echo "> [find_idle_profile] 현재 profile: $CURRENT_PROFILE"
 
-    if [ "$CURRENT_PROFILE" = "real1" ]; then
+    if [[ "$CURRENT_PROFILE" == "real1" ]]; then
         IDLE_PROFILE="real2"
-    elif [ "$CURRENT_PROFILE" = "real2" ]; then
+    elif [[ "$CURRENT_PROFILE" == "real2" ]]; then
         IDLE_PROFILE="real1"
     else
         echo "> [find_idle_profile] 알 수 없는 profile: '$CURRENT_PROFILE'. 기본값 'real2' 사용"
@@ -32,7 +33,7 @@ function find_idle_port()
 {
     IDLE_PROFILE=$(find_idle_profile)
 
-    if [ "$IDLE_PROFILE" = "real1" ]; then
+    if [[ "$IDLE_PROFILE" == "real1" ]]; then
         echo "8081"
     else
         echo "8082"
