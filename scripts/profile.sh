23c47c2793a739c54fecf8476ed1793ca5e5d1e2
SeoBYP <jajazxcv@naver.com>
2025-04-19 22:36:15 +0900

    테스트 빌드

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -30,8 +30,7 @@ function find_idle_port()
 {
     IDLE_PROFILE=$(find_idle_profile)
 
-    if [ ${IDLE_PROFILE} == real1 ]
-    then
+    if [ "$CURRENT_PROFILE" == "real1" ]; then
       echo "8081"
     else
       echo "8082"
