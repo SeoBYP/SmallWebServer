996adfd89f631a65bec7caea1938e839a0106f4f
SeoBYP <jajazxcv@naver.com>
2025-04-20 00:22:16 +0900

    로그인시 USER 권한 부여

--- src/main/java/com/myWebServer/application/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/myWebServer/application/config/auth/dto/OAuthAttributes.java
@@ -97,7 +97,7 @@ public class OAuthAttributes {
                 .name(name)
                 .email(email)
                 .picture(picture)
-                .role(Role.GUEST)
+                .role(Role.USER)
                 .build();
     }
 }
